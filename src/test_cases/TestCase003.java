e03878bf8b5872a78e2314b995049b3e4d3ec8dc
Temuulen0528 <Temuulen0528@gmail.com>
2022-04-02 15:50:28 -0400

    updated test case 003

--- src/test_cases/TestCase003.java
+++ src/test_cases/TestCase003.java
@@ -4,7 +4,9 @@ public class TestCase003 {
 
     //If you order Batman from Taobao you get Robin
 
-    //Robin is good but as good as Batman
+    //Robin is good but as good as Batman.
+
+    //Robin is mehhh ...
 
 
 }
